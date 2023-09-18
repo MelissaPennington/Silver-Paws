9d62bd00fcde27884208957e0b2d81831b9427c5
melissapennington <mlpennington9@gmail.com>
2023-09-18 18:40:44 -0500

    pet form is uniform

--- pages/deceased.js
+++ pages/deceased.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import Link from 'next/link';
-import { Button } from 'react-bootstrap';
 import { useAuth } from '../utils/context/authContext';
 import RememberPets from '../components/rememberPets';
 import { getDeletedPets } from '../api/petData'; // Import the correct function
@@ -21,9 +19,6 @@ function Deceased() {
 
   return (
     <div className="text-center my-4">
-      <Link passHref href="/pet/new">
-        <Button>Add A Pet</Button>
-      </Link>
       <div className="d-flex flex-wrap">
         {deceaseds.map((pet) => (
           <RememberPets
